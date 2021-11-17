7e08949839e525e9111b13463818712120745b31
Mark Dance <markpdance@yahoo.co.uk>
2021-11-17 08:36:52 +0000

    framework, lacking delete route and buttons

--- application/routes.py
+++ application/routes.py
@@ -8,17 +8,24 @@ def create_task():
     db.session.commit()
     return f"Task with id {new_task.id} added to database."
 
-# @app.route('/read')
-# def read():
-#     all_games = Games.query.all()
-#     games_string = ""
-#     for game in all_games:
-#         games_string += "<br>"+ game.name
-#     return games_string
+@app.route('/read/alltasks')
+def read_tasks():
+    all_tasks = Tasks.query.all()
 
-# @app.route('/update/<name>')
-# def update(name):
-#     first_game = Games.query.first()
-#     first_game.name = name
-#     db.session.commit()
-#     return first_game.name
+    tasks_dict = {"tasks": []}
+
+    for task in all_tasks:
+        tasks_dict["tasks"].append(
+          {
+              "description": task.description,
+              "completed": task.completed
+          }  
+        )
+    return tasks_dict
+
+@app.route('/update/task/<int:id>/<new_description>')
+def update_task(id, new_description):
+    task = Tasks.query.get(id)
+    task.description = new_description
+    db.session.commit()
+    return f"Task {id} updated to {new_description}"
